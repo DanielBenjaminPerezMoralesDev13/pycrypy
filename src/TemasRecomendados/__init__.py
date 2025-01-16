41d2e9cc093439e805163f00f397a8ca79c8bcb7
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 11:35:56 -0600

    First Commit

---
+++ src/TemasRecomendados/__init__.py
@@ -0,0 +1,4 @@
+# Author: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/DanielBenjaminPerezMoralesDev13
+# Gitlab: https://gitlab.com/DanielBenjaminPerezMoralesDev13
+# Email: danielperezdev@proton.me
