5b65008eff7c1922fff66d2ce6c2549155a3d1d7
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 13:19:49 -0600

    update

--- src/TemasRecomendados/palenight.py
+++ src/TemasRecomendados/palenight.py
@@ -1,7 +1,6 @@
 # Author: Daniel Benjamin Perez Morales
 # GitHub: https://github.com/DanielBenjaminPerezMoralesDev13
 # Gitlab: https://gitlab.com/DanielBenjaminPerezMoralesDev13
-# Gitlab: https://gitlab.com/DanielBenjaminPerezMoralesDev13
 # Email: danielperezdev@proton.me
 
 from typing import Dict, Any
